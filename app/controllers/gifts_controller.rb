f30017627e58bc569b424450be7ba3e0ad2cb868
Gong Ke Gouldstone <fifo@fifo-VirtualBox.(none)>
2013-11-29 05:38:22 -0500

    Added database and form for inputing information about a user.

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,22 @@
 class GiftsController < ApplicationController
 	def index
-		@gifts = Gift.all
+		@gift = Gift.new
 	end
+
+	def create
+        @gift = Gift.create( gift_params )
+        redirect_to gifts_path
+
+	end
+
+	private
+    def gift_params
+        params.
+        require(:gift).
+        permit(:name, :email_addr, :giver_gender, 
+        	:relationship, :occasion, :occasion_date, 
+        	:budget_min, :budget_max, :recipient_age_estimate,
+        	:recipient_gender, :recipient_info, :other_info)
+    end
+
 end
